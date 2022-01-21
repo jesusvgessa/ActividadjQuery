de6594716f4db9bef45c876da25672ec77bad33f
jesusvgessa <jesusvgessa@hotmail.com>
2022-01-21 18:05:35 +0100

    ej1

--- js/ej1.js
+++ js/ej1.js
@@ -1,26 +1,24 @@
 $(document).ready(function() {
     //Cojo todos los parrafos de los elementos de la lista
-    $("ul li label").hide();
+    $("p").hide();
 
     //Animaciones cuando entro en el elemento
-    $("ul li").mouseover(function(event) {
-        $(this).stop();
-        $(this).clearQueue();
-        $(this).children("label").show(1000);
-        //Cojo el elemento que tiene el puntero encima, y sus hermanos
+    $("li").mouseover(function(event) {
+        // $(this).stop();
+        // $(this).clearQueue();
+        $(this).children("p").show(1000);
+        //Cojo el elemento que tiene el puntero encima
         $(this).animate({
-            marginLeft: "2em"
+            marginLeft: "6em"
         }, 1000);
     });
 
     //Animaciones cuando salgo del elemento
-    $("ul li").mouseout(function(event) {
-        $(this).finish();
-        $(this).clearQueue();
-        $(this).children("label").hide(1000);
+    $("li").mouseout(function(event) {
+        $(this).children("p").hide(1000);
         //Le pongo en su posicion inicial
         $(this).animate({
-            marginLeft: "1em"
+            marginLeft: "4em"
         }, 1000);
     });
 });
