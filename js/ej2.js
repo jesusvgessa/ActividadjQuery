20fc2ae110271c75765219beeaaf892db08f2415
jesusvgessa <jesusvgessa@hotmail.com>
2022-01-23 12:53:30 +0100

    ej2

--- js/ej2.js
+++ js/ej2.js
@@ -1,15 +1,25 @@
 $(function() {
-    //Cojo el boton eliminar por su id, y le paso el evento onclick
+    //Cojo el boton insertar por su id, y le paso el evento onclick
     $("#inserta").on("click", function(event) {
-        if ($("table tr:last td").length > 7) {
+        console.log($("table tr:last td").length);
+        if ($("table tr:last td").length > 6) {
             $("table").append("<tr><td><img src='https://loremflickr.com/200/200'></td></tr>");
-            console.log("nueva fila");
         } else if ($("table tr:last td").length == 0) {
             $("<td><img src='https://loremflickr.com/200/200'></td>").appendTo("tr");
-            console.log("primera celda");
         } else {
             $("<td><img src='https://loremflickr.com/200/200'></td>").insertAfter("td:last");
-            console.log("seguimos");
+        } //Fin Si
+    });
+
+    //Cojo el boton eliminar por su id, y le paso el evento onclick
+    $("#elimina").on("click", function(event) {
+        if ($("table tr:last td").length == 1) {
+            $("td:last").remove();
+            if ($("table tr").length > 1) {
+                $("tr:last").remove();
+            } //Fin Si
+        } else {
+            $("td:last").remove();
         } //Fin Si
     });
 });
